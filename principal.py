4a76061163914038624476899a8ca8fe3317d8a7
Wellington Pereira <wellington@fieldlink.me>
2025-05-07 20:18:23 -0300

    Criar todos os sub-menus do sistema.

--- principal.py
+++ principal.py
@@ -1,28 +1,66 @@
-menu_principal = """[Menu Principal] Escolha uma das seguintes opções:
+menu_principal = """
+[Menu Principal] Escolha uma das seguintes opções:
 1 - Categorias
 2 - Editoras
 3 - Autores
 4 - Livros
-0 - Sair do programa"""
+0 - Sair do programa
+"""
 
-print(menu_principal)
+menu_categoria = """
+[Categorias] Escolha uma das seguintes opções:
+1 - Listar toda as categorias
+2 - Adicionar nova categoria
+3 - Excluir categoria
+4 - Ver categoria por Id
+0 - Voltar ao menu anterior
+"""
+
+menu_editora = """
+[Editoras] Escolha uma das seguintes opções:
+1 - Listar todas as editoras
+2 - Adicionar nova editora
+3 - Excluir editora
+4 - Ver editora por Id
+0 - Voltar ao menu anterior
+"""
+
+menu_autor = """
+[Autores] Escolha uma das seguintes opções:
+1 - Listar todos os autores
+2 - Adicionar novo autor
+3 - Excluir autor
+4 - Ver autor por Id
+5 - Editar autor
+0 - Voltar ao menu anterior
+"""
+
+menu_livro = """
+[Livros] Escolha uma das seguintes opções:
+1 - Listar todos os livros
+2 - Adicionar novo livro
+3 - Excluir livro
+4 - Ver livro por Id
+0 - Voltar ao menu anterior
+"""
 
 while True:
+    print(menu_principal)
     op = input('Digite a opção: ')
 
     if op == '0':
         break
     elif op == '1':
-        print('Opção escolhida: Categorias')
+        print(menu_categoria)
         input('Digite <ENTER> para continuar...')
     elif op == '2': # else if
-        print('Opção escolhida: Editoras')
+        print(menu_editora)
         input('Digite <ENTER> para continuar...')
     elif op == '3':
-        print('Opção escolhida: Autores')
+        print(menu_autor)
         input('Digite <ENTER> para continuar...')
     elif op == '4':
-        print('Opção escolhida: Livros')
+        print(menu_livro)
         input('Digite <ENTER> para continuar...')
     else:
         print('Opção inválida!')
