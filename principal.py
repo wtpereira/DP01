755583ad6cb4f1e4fe300d0ad6f956aac2f06aef
Wellington Pereira <wellington@fieldlink.me>
2025-05-07 21:59:22 -0300

    Iniciar a implementação da parte de Autores.

--- principal.py
+++ principal.py
@@ -44,6 +44,8 @@ menu_livro = """
 0 - Voltar ao menu anterior
 """
 
+tabela_autores = []
+
 while True:
     print(menu_principal)
     op = input('Digite a opção: ')
@@ -57,8 +59,34 @@ while True:
         print(menu_editora)
         input('Digite <ENTER> para continuar...')
     elif op == '3':
-        print(menu_autor)
-        input('Digite <ENTER> para continuar...')
+        while True:
+            print(menu_autor)
+            opcao_autor = input('Digite a opção: ')
+            if opcao_autor == '0':
+                break
+            elif opcao_autor == '1':
+                print('Nome | E-mail | Telefone | Biografia')
+                for autor in tabela_autores:
+                    print(f'{autor[0]} | {autor[1]} | {autor[2]} | {autor[3]}')
+            elif opcao_autor == '2':
+                nome_autor = input('Digite o nome do autor: ')
+                telefone_autor = input('Digite o telefone do autor: ')
+                bio_autor = input('Digite a biografia do autor: ')
+                email_autor = input('Digite o e-mail do autor: ')
+                novo_autor = []
+                novo_autor.append(nome_autor)
+                novo_autor.append(email_autor)
+                novo_autor.append(telefone_autor)
+                novo_autor.append(bio_autor)
+                tabela_autores.append(novo_autor)
+            elif opcao_autor == '3':
+                print('3')
+            elif opcao_autor == '4':
+                print('4')
+            elif opcao_autor == '5':
+                print('5')
+            else:
+                print('Opção inválida!')
     elif op == '4':
         print(menu_livro)
         input('Digite <ENTER> para continuar...')
