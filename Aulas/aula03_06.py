0b63df76ca75b2d0204acee063ae68d431dabfe2
Wellington Pereira <wellington@fieldlink.me>
2025-05-08 21:41:19 -0300

    Criar o arquivo aula03_06.py.

---
+++ Aulas/aula03_06.py
@@ -0,0 +1,26 @@
+# dicionário
+
+lista = [['Machado', 'm@m', 123, 'biom'], ['Clarice Lispector', 'clarice@teste', 11987, 'biografia da clarice']]
+
+machado = {
+    'nome': 'Machado',
+    'email': 'm@m',
+    'telefone': 123,
+    'biografia': 'biom'
+}
+
+clarice = {
+    'nome': 'Clarice Lispector',
+    'email': 'clarice@teste',
+    'telefone': 11987,
+    'biografia': 'biografia da clarice'
+}
+
+lista2 = [machado, clarice]
+print(lista2)
+
+print(lista[1][0])
+
+print(lista2[1]['Nome'])
+
+
