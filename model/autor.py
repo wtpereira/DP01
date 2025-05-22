387c32109c695f81274d3b3bb7c14d26d7ff2104
Wellington <wtpereira@gmail.com>
2025-05-22 20:35:28 -0300

    Adicionar anotações de tipo (typing annotations) na classe 'Autor'.

--- model/autor.py
+++ model/autor.py
@@ -5,39 +5,39 @@ class Autor:
     id_autor = 1
     __slots__ = ['__id', '__nome', '__email', '__telefone', '__biografia']
 
-    def __init__(self, n, e, t, b):  # construtor ou inicializador
+    def __init__(self, n: str, e: str, t: str, b: str):  # construtor ou inicializador
         self.id = Autor.id_autor
         self.nome = n
         self.email = e
         self.telefone = t
         self.biografia = b
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.id} | {self.nome} | {self.email} | {self.telefone} | {self.biografia}"
 
     @property
-    def id(self):
+    def id(self) -> int:
         return self.__id
 
     @id.setter
-    def id(self, i):
+    def id(self, i: int):
         self.__id = i
         Autor.id_autor += 1
 
     @property
-    def nome(self):
+    def nome(self) -> str:
         return self.__nome
 
     @nome.setter
-    def nome(self, n):
+    def nome(self, n: str):
         self.__nome = n.title()
 
     @property
-    def email(self):
+    def email(self) -> str:
         return self.__email
 
     @email.setter
-    def email(self, e):
+    def email(self, e: str):
         e = e.lower()  # Transformar para minúsculo.
         if email_valido(e):
             self.__email = e
@@ -47,18 +47,18 @@ class Autor:
             raise Exception('E-mail inválido!')
 
     @property
-    def telefone(self):
+    def telefone(self) -> str:
         return self.__telefone
 
     @telefone.setter
-    def telefone(self, t):
+    def telefone(self, t: str):
         self.__telefone = t
 
     @property
-    def biografia(self):
+    def biografia(self) -> str:
         return self.__biografia
 
     @biografia.setter
-    def biografia(self, bio):
+    def biografia(self, bio: str):
         self.__biografia = bio
 
