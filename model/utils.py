bf54d0fad04335defa1f5772af501e8afd8ac6a7
Wellington <wtpereira@gmail.com>
2025-05-22 21:39:42 -0300

    Usar 'lambda' para escrever a função 'email_valido'.

--- model/utils.py
+++ model/utils.py
@@ -1,5 +1,7 @@
-def email_valido(email):
-    if email.find('@') >= 1 and email.find('.com') >= 3:
-        return True
-    else:
-        return False
+# def email_valido(email):
+#     if email.find('@') >= 1 and email.find('.com') >= 3:
+#         return True
+#     else:
+#         return False
+
+email_valido = lambda email: True if (email.find('@') >= 1 and email.find('.com') >= 3) else False
