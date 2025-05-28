59733aa9a68ea33a1e3d100ef31def1f90244805
Wellington <wtpereira@gmail.com>
2025-05-27 21:31:11 -0300

    Terminar de codificar os arquivos do módulo Livro.

--- dao/livro_dao.py
+++ dao/livro_dao.py
@@ -8,10 +8,19 @@ class LivroDAO:
         LivroDAO.tabela_livros.append(novo_livro)
 
     def remover(self, id_livro):
-        pass
+        for index, livro in enumerate(LivroDAO.tabela_livros):
+            if livro.id == id_livro:
+                LivroDAO.tabela_livros.pop(index)
+                return True
+
+        return False
 
     def buscar_por_id(self, id_livro):
-        pass
+        for livro in LivroDAO.tabela_livros:
+            if livro.id == id_livro:
+                return livro
+
+        return None
 
     def editar(self, id_livro):
         pass
--- service/livro_service.py
+++ service/livro_service.py
@@ -5,7 +5,7 @@ from service.categoria_service import CategoriaService
 from service.editora_service import EditoraService
 
 menu_livro = """
-\033[0;34m[Livros] Escolha uma das seguintes opções:\033[0m
+\033[44m[Livros] Escolha uma das seguintes opções:\033[0m
 1 - Listar todos os livros
 2 - Adicionar novo livro
 3 - Excluir livro
@@ -98,8 +98,95 @@ class LivroService:
             input('\nPressione <ENTER> para continuar...\n')
 
     def remover(self):
-        pass
+        if not LivroService.livro_dao.listar():
+            print('Não existem livros cadastrados.')
+            input('\nPressione <ENTER> para continuar...\n')
+        else:
+            try:
+                id_livro = int(input('Digite o ID do livro a ser excluído: '))
+                if LivroService.livro_dao.remover(id_livro):
+                    print('Livro excluído com sucesso!')
+                else:
+                    print('ID não encontrado.')
+            except:
+                print('ID inválido!')
+
+            input('\nPressione <ENTER> para continuar...\n')
+
     def mostrar_por_id(self):
-        pass
+        if not LivroService.livro_dao.listar():
+            print('Não existem livros cadastrados.')
+            input('\nPressione <ENTER> para continuar...\n')
+        else:
+            try:
+                id_livro = int(input('Digite o ID do livro a ser consultado: '))
+                livro = LivroService.livro_dao.buscar_por_id(id_livro)
+                if livro:
+                    print('ID | Título | Ano | Qtde páginas | ISBN | Resumo | Autor | Categoria | Editora')
+                    print(livro)
+                else:
+                    print('ID não encontrado.')
+            except:
+                print('ID inválido!')
+
+            input('\nPressione <ENTER> para continuar...\n')
     def editar(self):
-        pass
+        if not LivroService.livro_dao.listar():
+            print('Não existem livros cadastrados.')
+            input('\nPressione <ENTER> para continuar...\n')
+        else:
+            if LivroService.autor_service.autor_dao.listar() == [] or LivroService.categoria_service.categoria_dao.listar() == [] or LivroService.editora_service.editora_dao.listar() == []:
+                print('É necessário pelo menos um autor, uma categoria, e uma editora cadastros.')
+                input('\nPressione <ENTER> para continuar...\n')
+            else:
+                try:
+                    while True:
+                        id_livro = int(input('Digite o ID do livro a ser editado: '))
+                        livro = LivroService.livro_dao.buscar_por_id(id_livro)
+                        if livro:
+                            break
+                        else:
+                            print('ID não encontrado.')
+
+                    titulo = input('Digite o título do livro: ')
+                    livro.titulo = titulo
+                    resumo = input('Digite o resumo do livro: ')
+                    livro.resumo = resumo
+                    ano = input('Digite o ano de publicação do livro: ')
+                    livro.ano = ano
+                    paginas = input('Digite a quantidade de páginas: ')
+                    livro.paginas = paginas
+                    isbn = input('Digite o ISBN do livro: ')
+                    livro.isbn = isbn
+
+                    while True:
+                        id_autor = int(input('Digite o ID do autor: '))
+                        autor = LivroService.autor_service.autor_dao.buscar_por_id(id_autor)
+                        if autor:
+                            livro.autor = autor
+                            break
+                        else:
+                            print('ID de autor não encontrado.')
+
+                    while True:
+                        id_categoria = int(input('Digite o ID da categoria: '))
+                        categoria = LivroService.categoria_service.categoria_dao.buscar_por_id(id_categoria)
+                        if categoria:
+                            livro.categoria = categoria
+                            break
+                        else:
+                            print('ID de categoria não encontrado.')
+
+                    while True:
+                        id_editora = int(input('Digite o ID da editora: '))
+                        editora = LivroService.editora_service.editora_dao.buscar_por_id(id_editora)
+                        if editora:
+                            livro.editora = editora
+                            break
+                        else:
+                            print('ID de editora não encontrado.')
+
+                    print('Livro editado com sucesso!')
+                    input('\nPressione <ENTER> para continuar...\n')
+                except Exception as ex:
+                    print(f'ID inválido: {ex}.')
